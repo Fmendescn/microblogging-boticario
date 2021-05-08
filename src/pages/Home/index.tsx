064f805c0ba630e0305560efcbadd274428a109b
Francisco Mendes <fmendes_cn@hotmail.com>
2021-05-08 10:47:02 -0300

    style: :heavy_plus_sign: add styled components to the project

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { Text } from 'react-native';
+
+import { Container } from './styles';
 
 const Home = (): JSX.Element => {
   return (
-    <View>
+    <Container>
       <Text>Home</Text>
-    </View>
+    </Container>
   );
 };
 
---
+++ src/pages/Home/styles.ts
@@ -0,0 +1,11 @@
+import styled from 'styled-components/native';
+
+export const Container = styled.View`
+  flex: 1;
+  background: #fff;
+
+  justify-content: center;
+  align-items: center;
+`;
+
+export default Container;
