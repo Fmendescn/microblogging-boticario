e3630b406d82a1d7065aeedf12fe67d3a49541f9
Francisco Mendes <fmendes_cn@hotmail.com>
2021-05-09 16:16:51 -0300

    style: :lipstick: render blog screen

--- src/pages/Blog/index.tsx
+++ src/pages/Blog/index.tsx
@@ -1,7 +1,21 @@
 import React from 'react';
 
-import { Container } from './styles';
+import PostCard from '../../components/PostCard';
+import { SectionTittle } from '../../shared/texts';
+import { Container, Header } from './styles';
 
-const Blog = (): JSX.Element => <Container />;
+const Blog = (): JSX.Element => (
+  <Container>
+    <Header>
+      <SectionTittle>Últimas novidades</SectionTittle>
+    </Header>
+    <PostCard
+      author="O boticario"
+      message="Além disso, nossos funcionários e familiares receberão kits de proteção. Afinal, o cuidado começa aqui dentro, né?"
+      date="2 dias atrás"
+      coverImage="https://images.unsplash.com/photo-1603993097397-89c963e325c7?ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80"
+    />
+  </Container>
+);
 
 export default Blog;
--- src/pages/Blog/styles.ts
+++ src/pages/Blog/styles.ts
@@ -1,3 +1,13 @@
 import styled from 'styled-components/native';
 
-export const Container = styled.View``;
+import colors from '../../shared/colors';
+
+export const Container = styled.View`
+  flex: 1;
+  background: ${colors.white};
+  padding: 22px;
+`;
+
+export const Header = styled.View`
+  margin: 16px 0 32px 0;
+`;
