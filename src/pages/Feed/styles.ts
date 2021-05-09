877015bad9008218362381948cb9854289fb8a65
Francisco Mendes <fmendes_cn@hotmail.com>
2021-05-09 12:00:11 -0300

    style: :lipstick: create Feed header

--- src/pages/Feed/styles.ts
+++ src/pages/Feed/styles.ts
@@ -1,19 +1,41 @@
 import styled from 'styled-components/native';
 
 import colors from '../../shared/colors';
+import { signOutIcon } from '../../shared/images';
 
 export const Container = styled.View`
   flex: 1;
-  background: #fff;
-
-  justify-content: center;
-  align-items: center;
+  background: ${colors.white};
+  padding: 22px;
 `;
 
-export const Tittle = styled.Text`
-  font-family: 'AvenirLTStd-Black';
-  font-size: 24px;
-  color: ${colors.mainColor};
+export const Header = styled.View`
+  margin: 16px 0 32px 0;
+  flex-direction: row;
+  justify-content: space-between;
 `;
 
-export default Container;
+export const Messages = styled.View`
+  flex: 1;
+`;
+
+export const GreetingMessage = styled.Text.attrs({
+  numberOfLines: 1,
+})`
+  font-family: 'AvenirLTStd-Book';
+  font-size: 18px;
+  color: ${colors.darkFont};
+  margin-bottom: 8px;
+`;
+
+export const SignOutButton = styled.TouchableOpacity``;
+
+export const SignOutIcon = styled.Image.attrs({
+  source: signOutIcon,
+  resizeMode: 'contain',
+})`
+  height: 30px;
+  width: 30px;
+
+  margin-left: 22px;
+`;
--- src/shared/images.ts
+++ src/shared/images.ts
@@ -4,5 +4,6 @@ const logo = require('../assets/images/logo.png');
 const homeIcon = require('../assets/images/home_icon.png');
 const bookIcon = require('../assets/images/book_icon.png');
 const addIcon = require('../assets/images/add_icon.png');
+const signOutIcon = require('../assets/images/logout_icon.png');
 
-export { logo, homeIcon, bookIcon, addIcon };
+export { logo, homeIcon, bookIcon, addIcon, signOutIcon };
